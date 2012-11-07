0d9b83e6a461578c42362e697c06ed1ac4570274
Joel Berger <joel.a.berger@gmail.com>
2012-11-07 10:21:11 -0600

    use += in python method

--- testing_greenstone_move.py
+++ testing_greenstone_move.py
@@ -28,38 +28,38 @@ for root, dirs, files in os.walk('.'):
 			for element in myElements:
 				value = element.attributes['name'].value
 				if value == 'dc.Title':
-					dcTitle = dcTitle + ', ' + element.firstChild.data
+					dcTitle += ', ' + element.firstChild.data
 				if value == 'dc.Creator':
-					dcCreator = dcCreator + ', ' + element.firstChild.data
+					dcCreator += ', ' + element.firstChild.data
 				if value == 'dc.Subject':
-					dcSubject = dcSubject + ', ' + element.firstChild.data
+					dcSubject += ', ' + element.firstChild.data
 				if value == 'dc.Description':
-					dcDescription = dcDescription + ', ' + element.firstChild.data
+					dcDescription += ', ' + element.firstChild.data
 				if value == 'dc.Publisher':
-					dcPublisher = dcPublisher + ', ' + element.firstChild.data
+					dcPublisher += ', ' + element.firstChild.data
 				if value == 'dc.Contributor':
-					dcContributor = dcContributor + ', ' + element.firstChild.data
+					dcContributor += ', ' + element.firstChild.data
 				if value == 'dc.Date':
-					dcDate = dcDate + ', ' + element.firstChild.data
+					dcDate += ', ' + element.firstChild.data
 				if value == 'dc.Type':
-					dcType = dcType + ', ' + element.firstChild.data
+					dcType += ', ' + element.firstChild.data
 				if value == 'dc.Format':
-					dcFormat = dcFormat + ', ' + element.firstChild.data
+					dcFormat += ', ' + element.firstChild.data
 				if value == 'dc.Identifier':
-					dcIdentifier = dcIdentifier + ', ' + element.firstChild.data
+					dcIdentifier += ', ' + element.firstChild.data
 				if value == 'dc.Source':
-					dcSource = dcSource + ', ' + element.firstChild.data
+					dcSource += ', ' + element.firstChild.data
 				if value == 'dc.Language':
-					dcLanguage = dcLanguage + ', ' + element.firstChild.data
+					dcLanguage += ', ' + element.firstChild.data
 				if value == 'dc.Relation':
-					dcRelation = dcRelation + ', ' + element.firstChild.data
+					dcRelation += ', ' + element.firstChild.data
 				if value == 'dc.Coverage':
-					dcCoverage = dcCoverage + ', ' + element.firstChild.data
+					dcCoverage += ', ' + element.firstChild.data
 				if value == 'dc.Rights':
-					dcRights = dcRights + ', ' + element.firstChild.data
+					dcRights += ', ' + element.firstChild.data
 
 			sqlChunk = "(" + "'" + dcTitle[2:] + "', " + "'" + dcCreator[2:] + "', " + "'" + dcSubject[2:] + "', " + "'" + dcDescription[2:] + "', " + "'" + dcPublisher[2:] + "', " + "'" + dcContributor[2:] + "', " + "'" + dcDate[2:] + "', " + "'" + dcType[2:] + "', " + "'" + dcFormat[2:] + "', " + "'" + dcIdentifier[2:] + "', " + "'" + dcSource[2:] + "', " + "'" + dcLanguage[2:] + "', " + "'" + dcRelation[2:] + "', " + "'" + dcCoverage[2:] + "', " + "'" + dcRights[2:] + "')" 
-			allSql = allSql + ", " + sqlChunk
+			allSql += ", " + sqlChunk
 
 completeSql = "INSERT INTO dcRecords VALUES" + allSql[2:] + ";"
 
